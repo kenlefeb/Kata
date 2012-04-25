94da8c074d9df14c6b511e3146c52af634386d0d
unknown <klefebvre@KLEFEBVRE-01.ba.office>
2012-04-25 00:02:03 -0400

    Completed the String Calculator Kata (http://osherove.com/tdd-kata-1/)

--- string calculator/Calculator/Calculator.cs
+++ string calculator/Calculator/Calculator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,72 @@ namespace LeFebvre.Kata.StringCalculator
 {
     public class Calculator
     {
+        public Calculator() : this(new string[] { "," }) { }
+
+        public Calculator(string[] delimiters)
+        {
+            this.delimiters = delimiters;
+        }
+
+        private string[] delimiters;
+
         public int Add(string numbers)
         {
+            defineDelimiters(ref numbers);
+
             if (string.IsNullOrEmpty(numbers))
                 return 0;
 
-            var values = numbers.Split(new char[] { ',' }).Select<string, int>(item => int.Parse(item));
+            var values = parseNumbers(numbers);
+            checkForNegatives(values);
+
             return values.Sum();
+        }
+
+        private IEnumerable<int> parseNumbers(string numbers)
+        {
+            var strings = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var values = numbers.Split(this.delimiters, StringSplitOptions.RemoveEmptyEntries)
+                            .Select<string, int>(item => int.Parse(item))
+                            .Where(value => value <= 1000);
+            return values;
+        }
+
+        private void defineDelimiters(ref string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var newline = numbers.IndexOf('\n');
+                if (numbers.Substring(2).StartsWith("[") && numbers.Substring(newline - 1).StartsWith("]"))
+                {
+                    var delimiterDefinitions = numbers.Substring(3, (newline - 4));
+                    delimiters = delimiterDefinitions.Split(new string[] { "][" }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                else
+                {
+                    var strings = new List<string>();
+                    foreach (var character in numbers.Substring(2, (newline - 2)).ToCharArray())
+                        strings.Add(character.ToString());
+                    delimiters = strings.ToArray();
+                }
+                numbers = numbers.Substring(newline + 1);
+            }
+        }
+
+        private static void checkForNegatives(IEnumerable<int> values)
+        {
+            var negatives = values.Where(nbr => nbr < 0);
+            if (negatives.Count() > 0)
+            {
+                var builder = new StringBuilder();
+                foreach (var negative in negatives)
+                {
+                    if (builder.Length > 0)
+                        builder.Append(", ");
+                    builder.Append(negative);
+                }
+                throw new Exception(string.Format("Negatives not allowed: {0}", builder.ToString()));
+            }
         }
     }
 }
--- string calculator/Tests/Design_Tests.cs
+++ string calculator/Tests/Design_Tests.cs
@@ -73,5 +73,140 @@ namespace Tests.LeFebvre.Kata.StringCalculator
             // Assert
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void Add_method_accepts_unknown_number_of_numbers()
+        {
+            // Allow the Add method to handle an unknown amount of numbers
+
+            // Arrange
+            var randomNumber = new Random().Next(1, 100);
+            var numbersList = new List<int>(randomNumber);
+            var builder = new StringBuilder();
+            for (var index = 0; index < randomNumber; index++)
+            {
+                var thisNumber = new Random().Next(0, 100);
+                numbersList.Add(thisNumber);
+                if (builder.Length > 0)
+                    builder.Append(",");
+                builder.Append(thisNumber);
+            }
+            var numbers = builder.ToString();
+            var expected = numbersList.Sum();
+
+            // Act
+            var actual = new Calculator().Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_method_accepts_newline_delimiters()
+        {
+            // Arrange
+            var numbers = "1\n2";
+            var expected = 3;
+
+            // Act
+            var actual = new Calculator(new string[] { ",", "\n" }).Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_method_accepts_optional_delimiter_line()
+        {
+            // Support different delimiters to change a delimiter, 
+            // the beginning of the string will contain a separate line that looks like this:   
+            // “//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’ . 
+            // the first line is optional. all existing scenarios should still be supported
+
+            // Arrange
+            var numbers = "//;\n1;2";
+            var expected = 3;
+
+            // Act
+            var actual = new Calculator().Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_method_throws_exception_when_negatives_are_passed()
+        {
+            // Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed.
+            // if there are multiple negatives, show all of them in the exception message 
+
+            // Arrange
+            var numbers = "-2,-3";
+            var expected = "Negatives not allowed: -2, -3";
+            var actual = string.Empty;
+
+            // Act
+            try
+            {
+                new Calculator().Add(numbers);
+            }
+            catch (Exception ex)
+            {
+                actual = ex.Message;
+            }
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_method_ignores_numbers_higher_that_1000()
+        {
+            // Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2
+
+            // Arrange
+            var numbers = "2,1001";
+            var expected = 2;
+
+            // Act
+            var actual = new Calculator().Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Delimiters_may_be_any_length()
+        {
+            // Delimiters can be of any length with the following format: 
+            // “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6
+
+            // Arrange
+            var numbers = "//[***]\n1***2***3";
+            var expected = 6;
+
+            // Act
+            var actual = new Calculator().Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Multiple_delimiters_may_be_any_length()
+        {
+            // Allow multiple delimiters like this: 
+            // “//[delim1][delim2]\n” for example “//[*][%]\n1*2%3” should return 6.
+
+            // Arrange
+            var numbers = "//[*][%]\n1*2%3";
+            var expected = 6;
+
+            // Act
+            var actual = new Calculator().Add(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
